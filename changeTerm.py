812414a857b5271db970aa7cac267deede1924b1
Dominique Charlebois <domo.charlebois@gmail.com>
2020-07-15 22:33:53 -0700

    Update changeTerm.py, Add documentation, Cleanup Code

--- changeTerm.py
+++ changeTerm.py
@@ -1,10 +1,24 @@
+# Import UVic Class from uvic.py
 import uvic
 
-def main():
+# Change Term
+def changeTerm():
+  """
+  Change Selected Term for UVic Schedule. 
+
+  Parameters None
+  Returns None
+  """
+
+  # Authorize with UVic
   auth = uvic.Auth()
 
-  # Attempt to open the MyCard page
+  # Load to Change Term Page
   auth.load('https://www.uvic.ca/BAN1P/bwskflib.P_SelDefTerm?calling_proc_name=bwskfshd.P_CrseSchdDetl')
+
+# Main
+def main():
+  changeTerm()
 
 if __name__ == "__main__":
   main()
